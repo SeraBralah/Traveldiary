348ffa37e962abba5869ee726966ff97c61ca448
fontain58u <clara.fontaine3@etu.univ-lorraine.fr>
2024-05-06 11:30:52 +0200

    Vérification des dates

--- src/carnetdevoyage/carnet/presentation/PresentationCarnet.java
+++ src/carnetdevoyage/carnet/presentation/PresentationCarnet.java
@@ -23,15 +23,62 @@ public class PresentationCarnet {
         this.titre = titre;
     }
 
+    /**
+     * Fixe la date de début du voyage
+     * @param datedebut
+     * @throws DateException
+     */
     public void setDatedebut(String datedebut) throws DateException {
         try {
             LocalDate date = LocalDate.parse(datedebut, modeledate);
+            if(!verifDate(datedebut)) throw new DateException("La date est invalide");
             this.datedebut = datedebut;
         } catch (DateTimeParseException e) {
             throw new DateException("La date est incorrecte");
         }
     }
 
+    /**
+     * S'assure que la date rentrée est valide par rapport au calendrier
+     * @param date
+     * @return
+     * @throws DateException
+     */
+    public boolean verifDate(String date) throws DateException {
+        String[] datesplit = date.split("/");
+        int jour = Integer.parseInt(datesplit[0]);
+        int mois = Integer.parseInt(datesplit[1]);
+        int annee = Integer.parseInt(datesplit[2]);
+        if(1900>=annee) throw new DateException("Vous n'êtes pas si vieux !");
+        if(annee>LocalDate.now().getYear()) throw new DateException("Vous possédez un tardis ?");
+        if(mois==2) {
+            if (annee % 4 == 0) {
+                if (0 >= jour || jour >= 30) {
+
+                    return false;
+                }
+            } else if (0 >= jour || jour >= 29) return false;
+        }
+        if(mois%2==0 ){
+            if(0>=jour || jour>=31){ //jour plus petit que 0 ou plus grand que 31
+                return false;
+            }
+        } else { //mois impair, février et août
+            if (0 >= jour || jour >= 32) {
+                return false;
+            }
+
+        }
+return true;
+    }
+
+    /**
+     * Vérifie que la date de début et la date de fin sont bien conformes
+     * @param datedebut
+     * @param datefin
+     * @return
+     * @throws DateException
+     */
     public boolean dateValide(String datedebut, String datefin) throws DateException {
         try {
             LocalDate debut = LocalDate.parse(datedebut, modeledate);
@@ -42,9 +89,15 @@ public class PresentationCarnet {
         }
     }
 
+    /**
+     * Fixe la date de la fin du voyage
+     * @param datefin
+     * @throws DateException
+     */
     public void setDatefin(String datefin) throws DateException{
         try{
             LocalDate fin = LocalDate.parse(datefin, modeledate);
+            if(!verifDate(datefin)) throw new DateException("La date est invalide");
             this.datefin = datefin;
         } catch (DateTimeParseException e) {
             throw new DateException("La date est incorrecte");
---
+++ tests/PresentationCarnetTest.java
@@ -0,0 +1,45 @@
+import carnetdevoyage.carnet.presentation.PresentationCarnet;
+import carnetdevoyage.exceptions.DateException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PresentationCarnetTest {
+
+
+
+
+    @Test
+    void setDatedebutValide() throws DateException {
+        PresentationCarnet carnet = new PresentationCarnet("test");
+        carnet.setDatedebut("02/02/2024");
+        assertSame(carnet.getDatedebut(),"02/02/2024");
+    }
+
+    @Test
+    void setDatedebutNonValide() throws DateException {
+        PresentationCarnet carnet = new PresentationCarnet("Test");
+        assertThrows(DateException.class,()-> carnet.setDatedebut("30/02/2024"));
+        assertThrows(DateException.class,()-> carnet.setDatedebut("29/02/2023"));
+        assertThrows(DateException.class,()-> carnet.setDatedebut("29/15/2023"));
+        assertThrows(DateException.class,()-> carnet.setDatedebut("58/15/2023"));
+        assertThrows(DateException.class,()-> carnet.setDatedebut("17/02/2029"));
+    }
+
+    @Test
+    void dateValide() throws DateException {
+        PresentationCarnet carnet = new PresentationCarnet("Test");
+        carnet.setDatedebut("02/04/2023");
+        carnet.setDatefin("05/04/2023");
+        assertTrue(carnet.dateValide(carnet.getDatedebut(),carnet.getDatefin()));
+    }
+
+    @Test
+    void dateNonValide() throws DateException {
+        PresentationCarnet carnet = new PresentationCarnet("Test");
+        carnet.setDatedebut("10/04/2023");
+        carnet.setDatefin("01/03/2023");
+        assertFalse(carnet.dateValide(carnet.getDatedebut(),carnet.getDatefin()));
+    }
+
+}
